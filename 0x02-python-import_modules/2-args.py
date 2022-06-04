94e024f46fa9d2f39f85c565213d1f7ff83205e6
mardoche01 <mardoche.gneran@gmail.com>
2022-06-04 04:22:02 -0700

    task 2 update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
     element = len(sys.argv) - 1
     if element == 0:
         print("0 arguments.")
     elif element == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(element))
     for i in range(element):
