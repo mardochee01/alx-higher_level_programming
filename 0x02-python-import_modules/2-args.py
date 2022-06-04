7d2f95a7a58c82008d05e832ef3c900a6a6247b8
mardoche01 <mardoche.gneran@gmail.com>
2022-06-04 03:55:59 -0700

    tas 2 completed

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    element = len(sys.argv) - 1
+    if element == 0:
+        print("{} argument.".format(element))
+    else:
+        print("{} argument:".format(element))
+    for i in range(element):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
