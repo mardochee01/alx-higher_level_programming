f5d5b8da76f94f76bd1c805213689a6fffb03aa9
mardochee01 <mardoche.gneran@gmail.com>
2022-06-11 07:25:21 -0700

    task 6 update

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     for i in sorted(a_dictionary):
-        print("{}: {}".format(i, a_dictionary[i])) 
+        print("{:s}: {}".format(i, a_dictionary[i])) 
