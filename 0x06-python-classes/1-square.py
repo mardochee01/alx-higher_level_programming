4d00d49555c2708fe0c356cffc67cd6d99437bf0
mardochee01 <mardoche.gneran@gmail.com>
2022-06-21 21:35:08 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/puyhon3
+"""Define a Class Sqaure."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize with size
+        Args:
+            size(int): Private instance attribute.
+        """
+        self.__size = size
