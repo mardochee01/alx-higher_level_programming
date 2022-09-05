4671fc05d654235abf7ba8ab4f7f9a638969dbe5
mardochee01 <mardoche.gneran@gmail.com>
2022-09-05 17:14:08 +0000

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (!isNaN(process.argv[2])) {
+  const num = parseInt(process.argv[2]);
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
