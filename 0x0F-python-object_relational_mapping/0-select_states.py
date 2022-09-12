c0a252a73a06200abd8e198bd2ae63e0e2c31d71
mardochee01 <mardoche.gneran@gmail.com>
2022-09-12 10:02:46 +0000

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=db_user, passwd=db_password, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    data = cur.fetchall()
-    for states in data:
-        print(states)
+    cur.execute("SELECT * FROM `states` ORDER BY `states.id`")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close
     db.close
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,9 +12,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host ="localhost", port=3306, 
                          user=db_user, passwd=db_password, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY `states.id` WHERE `states.names` LIKE 'N%'")
-    data = cur.fetchall()
-    for states in data:
-        print(states)
+    cur.execute("SELECT * FROM `states` ORDER BY `states.id`")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] is 'N':
+            print(row)
     cur.close()
     db.close()
