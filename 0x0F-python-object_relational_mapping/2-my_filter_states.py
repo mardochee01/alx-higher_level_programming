f23cb39b103a91af602a93dc55ed284b6994ade2
mardochee01 <mardoche.gneran@gmail.com>
2022-09-12 10:51:54 +0000

    task update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-"""script that takes in an argument 
-and displays all values in the states table 
+"""script that takes in an argument
+and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
 """
 
