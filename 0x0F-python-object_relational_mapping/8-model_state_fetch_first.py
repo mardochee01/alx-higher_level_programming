4edc144fd39aa0be866247560fd8ae03c366d4e9
mardochee01 <mardoche.gneran@gmail.com>
2022-09-14 13:13:06 +0000

    update

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(db_user, db_password,
                                    db_name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
